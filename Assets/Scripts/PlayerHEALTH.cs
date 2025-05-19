d67d741f63b193cc8681031186345d0cb43387d0
UOC\2407112 <harrisonfwoollard@gmail.com>
2025-05-19 10:30:59 +0100

    added idle shotgun sprite and removed the PLACEHOLDER shotgun. Current shotgun is an empty game object

---
+++ Assets/Scripts/PlayerHEALTH.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHEALTH : MonoBehaviour
+{
+    float hP = 100f;
+
+    private void Update()
+    {
+        print(hP);
+    }
+
+    public void TakeDamage(float enemyBullets)
+    {
+        hP -= enemyBullets;
+        if(hP <= 0)
+        {
+            Time.timeScale = 0;
+        }
+    }
+}
