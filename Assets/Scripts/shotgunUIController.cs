6e6a395a27a04cdf36b1df9cf4b79f39cc47c644
UOC\2407112 <harrisonfwoollard@gmail.com>
2025-05-19 10:51:40 +0100

    Shotgun is animated with the 2D sprites

---
+++ Assets/Scripts/shotgunUIController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class shotgunUIController : MonoBehaviour
+{
+    //Thank you ChatGPT for this code
+
+    public Image shotgunImage;
+
+    public Sprite shotgunIdle;
+    public Sprite shotgunFire1;
+    public Sprite shotgunFire2;
+    public float fireDisplayTime = 0.15f;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            StartCoroutine(FireShotgun());
+        }
+    }
+
+    IEnumerator FireShotgun()
+    {
+        shotgunImage.sprite = shotgunFire1;
+
+        yield return new WaitForSeconds(fireDisplayTime);
+        shotgunImage.sprite = shotgunFire2;
+
+        yield return new WaitForSeconds(fireDisplayTime);
+        shotgunImage.sprite = shotgunIdle;
+
+    }
+}
