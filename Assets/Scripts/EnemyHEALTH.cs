c0537cf8f67f9d7c0870e666193a12d95f1fe146
UOC\2407112 <harrisonfwoollard@gmail.com>
2025-05-19 09:28:33 +0100

    added rudimentary scripts for the weapon and Enemy Health, an enemy has yet to be implimented

---
+++ Assets/Scripts/EnemyHEALTH.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHEALTH : MonoBehaviour
+{
+    float hP = 100f;
+
+    bool isDead = false;
+
+    public bool IsDead()
+    {
+        return isDead;
+    }
+
+    public void TakeDamage(float weaponDamage)
+    {
+        BroadcastMessage("OnDamageTaken");
+        hP -= weaponDamage;
+        if(hP <= 0)
+        {
+            if (isDead) { return; }
+            isDead = true;
+            //GetComponent<Animator>().SetTrigger("dyingTrigger");
+        }
+    }
+}
---
+++ Assets/Scripts/weaponFPS.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weaponFPS : MonoBehaviour
+{
+    [SerializeField] Camera originPos;
+
+    [SerializeField] float weaponRange;
+    [SerializeField] float weaponDamage;
+    [SerializeField] float cooldownTime = 0.1f;
+
+    bool canShoot = true;
+
+    private void OnEnable()
+    {
+        canShoot = true;
+    }
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0) && canShoot)
+        {
+            StartCoroutine(Shoot());
+        }
+    }
+
+    private IEnumerator Shoot()
+    {
+        canShoot = false;
+
+        ProcessRaycast();
+
+        yield return new WaitForSeconds(cooldownTime);
+        canShoot = true;
+    }
+
+    private void ProcessRaycast()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(originPos.transform.position, originPos.transform.forward, out hit, weaponRange))
+        {
+            EnemyHEALTH enemyHealth = hit.transform.GetComponent<EnemyHEALTH>();
+            if (enemyHealth == null)
+            {
+                return;
+            }
+            enemyHealth.TakeDamage(weaponDamage);
+        }
+
+        else
+        {
+            return;
+        }
+    }
+
+    
+}
+    
+
