7ae7f9ac2d6ec66dc71eb03364e1e3f73e0fd68d
UOC\2407112 <harrisonfwoollard@gmail.com>
2025-05-27 15:13:27 +0100

    Trying to fix broken Voodoo Doll weapon. It is animated, though the sprite is too big and cant currently detect the enemies. Shotgun is disabled currently

--- Assets/Scripts/VoodooDollWeapon.cs
+++ Assets/Scripts/VoodooDollWeapon.cs
@@ -12,8 +12,91 @@ public class VoodooDollWeapon : MonoBehaviour
 
 
     [Header("Attack Settings")]
-    public float attackRange = 25f;
+    public float attackRange = 100f;
     public int damage = 5;
     public float attackCooldown = 0.5f;
     public float stabAnimDuration = 0.3f;
+
+    [Header("References")]
+    public Transform firePoint;
+    public LayerMask enemyLayer;
+    public PlayerHEALTH playerHealth; //This is a bit confusing
+
+    private SpriteRenderer spriteRenderer;
+
+    private float lastAttackTime = -999f;
+    private bool isAttacking = false;
+    private float stabAnimTimer = 0f;
+    private bool stabSpriteToggle = false;
+
+    public void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = idleSprite;
+    }
+
+    public void Update()
+    {
+        if(Input.GetButtonDown("Fire1") && Time.time >= lastAttackTime + attackCooldown)
+        {
+            Attack();
+        }
+
+        if (isAttacking)
+        {
+            HandleStabAnimation();
+        }
+    }
+
+    public void Attack()
+    {
+        lastAttackTime = Time.time;
+        isAttacking = true;
+        stabAnimTimer = 0f;
+        stabSpriteToggle = false;
+
+        RaycastHit2D hit = Physics2D.Raycast(firePoint.position, firePoint.right, attackRange, enemyLayer);
+
+        Debug.DrawRay(firePoint.position, firePoint.right * attackRange, Color.red, 1f); // <--- remove later when fixed
+
+        if(hit.collider != null)
+        {
+            //Hit enemy
+            Debug.Log("Hit: " + hit.collider.name);
+            EnemyHEALTH enemy = hit.collider.GetComponent<EnemyHEALTH>();
+            if (enemy != null)
+            {
+                enemy.TakeDamage(damage);
+            }
+        }
+        else
+        {
+            Debug.Log("No enemy Hit! Damaging Player.");
+            playerHealth.TakeDamage(damage);
+        }
+    }
+
+    public void HandleStabAnimation()
+    {
+        stabAnimTimer += Time.deltaTime;
+
+        //Switch sprite halfway through the animation stabbing effect
+
+        if(stabAnimTimer < stabAnimDuration / 2f)
+        {
+            spriteRenderer.sprite = stabSprite1;
+        }
+
+        else if (stabAnimTimer < stabAnimDuration)
+        {
+            spriteRenderer.sprite = stabSprite2;
+        }
+        else
+        {
+            //Animation finsihed, back to the idle animation
+            spriteRenderer.sprite = idleSprite;
+            isAttacking = false;
+        }
+    }
+
 }
