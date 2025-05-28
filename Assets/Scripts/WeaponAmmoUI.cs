936c0f70c3302ea0a2aac4dd95dadd114c0101fe
UOC\2407112 <harrisonfwoollard@gmail.com>
2025-05-28 11:12:03 +0100

    initial scripts for reloading added, still needs more work

---
+++ Assets/Scripts/WeaponAmmoUI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class WeaponAmmoUI : MonoBehaviour
+{
+    public weaponAmmo weaponAmmoButLikeNotTheScript;
+    public TMP_Text ammoText;
+
+    public void Update()
+    {
+        if (weaponAmmoButLikeNotTheScript != null && ammoText != null)
+        {
+            ammoText.text = weaponAmmoButLikeNotTheScript.currentAmmo + " / " + weaponAmmoButLikeNotTheScript.maxAmmo;
+        }
+    }
+}
