ec04b97248615334376163bf7851216c293fbd7a
UOC\2407112 <harrisonfwoollard@gmail.com>
2025-05-30 09:42:18 +0100

    added enemy ai script and projectile scrip plus prefab. currently the enemies still won't shoot of persue the player

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    //gonna be graduating with my guy ChatGPT by the end of this
+    //Suffice to say I am still the one typing all this out, not blindly copy pasting
+
+    public Transform player;
+    public float chaseRange = 15f;
+    public float shootingRange = 10f;
+    public float fireRate = 1f;
+    public GameObject projectilePrefab;
+    public Transform firePointForBullet;
+    public float projectileSpeed = 25f;
+    public float damage = 10f;
+
+    private NavMeshAgent agent;
+    private float nextFireTime;
+
+    public void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    public void Update()
+    {
+        if (!player) return;
+
+        float distance = Vector3.Distance(transform.position, player.position);
+
+        if(distance > chaseRange)
+        {
+            agent.isStopped = true;
+            return;
+        }
+
+        if (distance > shootingRange)
+        {
+            //Chase the player
+            agent.isStopped = false;
+            agent.SetDestination(player.position);
+        }
+        else
+        {
+            //stop moving and begin shooting
+            agent.isStopped = true;
+            transform.LookAt(player);
+
+            if(Time.time >= nextFireTime)
+            {
+                Shoot();
+                nextFireTime = Time.time + 1f / fireRate;
+            }
+        }
+    }
+
+    public void Shoot()
+    {
+        GameObject proj = Instantiate(projectilePrefab, firePointForBullet.position, firePointForBullet.rotation);
+        Rigidbody rb = proj.GetComponent<Rigidbody>();
+
+        if (rb)
+        {
+            rb.velocity = (player.position - firePointForBullet.position).normalized * projectileSpeed;
+        }
+
+        //remeber create projectile script
+        Projectile projectileScript = proj.GetComponent<Projectile>();
+        if (projectileScript)
+        {
+            projectileScript.damage = damage;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float damage = 10f;
+    public float lifetime = 5f;
+
+    public void Start()
+    {
+        Destroy(gameObject, lifetime); // in case bullet projectile doesn't hit anything
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerHEALTH health = other.GetComponent<PlayerHEALTH>();
+            if (health)
+            {
+                health.TakeDamage(damage);
+            }
+            Destroy(gameObject);
+        }
+    }
+
+}
